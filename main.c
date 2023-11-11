ea1b184998ccafe88197ac58dc0b570d55dd7aa5
Ubuntu <ubuntu@ip-172-31-1-168.us-east-2.compute.internal>
2023-11-11 23:58:26 +0000

    Realizamos las funciones en el main para utilizar la librería

--- main.c
+++ main.c
@@ -1,3 +1,29 @@
-int main(){
+#include <stdio.h>
+#include "libpilagrange.h"
 
+int main(){
+   float x[100], y[100], xp, yp;
+    int i, n;
+
+
+    printf("Ingrese el número de datos: ");
+    scanf("%d", &n);
+
+    printf("Ingrese los datos:\n");
+    for (i = 0; i < n; i++) {
+        printf("x[%d] = ", i);
+        scanf("%f", &x[i]);
+        printf("y[%d] = ", i);
+        scanf("%f", &y[i]);
+    }
+
+    printf("Ingrese el punto de interpolación: ");
+    scanf("%f", &xp);
+
+    lagrange(x, y, n, xp, &yp);
+
+    printf("El valor interpolado en %.3f es %.3f.", xp, yp);
+
+    getchar();
+    return 0;
 }
